6067e5c97c0521cd8aa7893862f8916382c2521a
jazit97 <79907613+jazit97@users.noreply.github.com>
2022-06-30 09:25:48 +0200

    SetPortValue

--- Assets/GforceMockupWrapper.cs
+++ Assets/GforceMockupWrapper.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.OpenXR.Features.Interactions;
 
 public class GforceMockupWrapper : MonoBehaviour
 {
-    private SerialConnection sc;
     private int xForce = 10;
     private int yForce = 9;
     private int zForce = -5;
-    private int writesPerSecond = 1;
+    private float writesPerSecond = 1f;
     private float timeSinceLastUpdate = 0.0f;
-    void Start()
-    {
-        sc = SerialConnection.instance;
-    }
-
+    
     // Update is called once per frame
     void Update()
+    {
+        //sendForces();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SerialConnection.setOutputOnPort(SerialConnection.LED_PORT, SerialConnection.HIGH);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SerialConnection.setOutputOnPort(SerialConnection.LED_PORT, SerialConnection.LOW);
+        }
+    }
+
+    private void sendForces()
     {
         timeSinceLastUpdate += Time.deltaTime;
         if (timeSinceLastUpdate >= writesPerSecond)
         {
-            string send = "*" + xForce.ToString() + ":" + yForce.ToString() + ":" + zForce.ToString() + ";";
-            sc.Send(send);
+            string send = xForce.ToString() + ":" + yForce.ToString() + ":" + zForce.ToString() + ";";
+            timeSinceLastUpdate = 0f;
+            SerialConnection.Send(send);
         }
     }
 }
--- Assets/Scripts/SerialConnection.cs
+++ Assets/Scripts/SerialConnection.cs
@@ -3,51 +3,100 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
+using System.Threading;
+using HTC.UnityPlugin.Vive;
 using UnityEngine;
+using UnityEngine.XR.OpenXR.Features.Interactions;
 
 public class SerialConnection : MonoBehaviour
 {
-    public static SerialConnection instance;
-    private static SerialPort sp;
+    #region constants
+    public const int MOTOR1_PORT = 0;
+    public const int MOTOR2_PORT = 1;
+    public const int MOTOR3_PORT = 2;
+    public const int LED_PORT = 13;
+    public const int HIGH = 1;
+    public const int LOW = 0;
+    private const string setOperationChar = ">";
+    #endregion
 
+    #region  exposedVariables
+    public string portName = "COM3";
+    public int baudRate = 9600;
+    public bool readFromSerial = true;
+    #endregion
 
-    private const string portName = "COM3";
-
-    private const int baudRate = 9600;
-
-    // Start is called before the first frame update
-    private const float readIntervalperSecond = 0.3f;
+    #region privateMembers
+    private static SerialPort stream;
     private float lastRead;
-    
+    private Thread readThread;
+    #endregion
 
+    #region UnityLifecycle
     private void Awake()
     {
-        sp = new SerialPort(portName, baudRate);
-        instance = this;
-        sp.Open();
-        sp.ReadTimeout = 200;
+        //configure and open serial port
+        stream = new SerialPort(portName, baudRate);
+        stream.ReadTimeout = 500;
+        stream.WriteTimeout = 500;
+        stream.Open();
+        
+        //start the read thread
+        readThread = new Thread(read);
+        readThread.Start();
     }
-
     private void Update()
     {
+        
     }
-
-
-    public void Send(string s)
-    {
-        sp.Write(s);
-    }
-
-    public String readExisting()
-    {
-        string value = null; 
-        value = sp.ReadExisting();
-        return value;
-
-    }
-
     private void OnDestroy()
     {
-        sp.Close();
+        stream.Close();
     }
+    #endregion
+    
+    #region communicationFunctions
+    public static void Send(string s)
+    {
+        stream.Write(s);
+        Debug.Log("Send value: " + s + "over serialconnection");
+    }
+
+    public void read()
+    {
+        while (true)
+        {
+            if (readFromSerial)
+            {
+                try
+                {
+                    string value = "";
+                    value = stream.ReadLine();
+                    if (value != "") Debug.Log("Serialport: " + value);
+                }
+                catch (TimeoutException)
+                {
+                    //timeout will happen from time to time
+                    //since we only use it to log the state of the arduino to console we can ignore it
+                }
+            }
+        }
+    }
+
+    public static void setOutputOnPort(int port, int value)
+    {
+        if (port != LED_PORT && port != MOTOR1_PORT && port != MOTOR2_PORT && port != MOTOR3_PORT)
+        {
+            throw new ArgumentException("Port '" + port +"' is no valid port");
+        }
+
+        if (value != LOW && value != HIGH)
+        {
+            throw new ArgumentException("Value '" + value  +"' is no valid Value");
+        }
+        Debug.Log("Writing: " + setOperationChar + port + ":" + value);
+        stream.WriteLine(setOperationChar + port + ":" + value);
+    }
+    #endregion
+    
 }
