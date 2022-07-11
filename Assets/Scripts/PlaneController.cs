2c6c828af59686a48ee4b144eeb721465e06817a
KattlerJoe <paka0010@stud.hs-kl.de>
2022-07-11 09:12:54 +0200

    gotta catch all the conflicts
    merchconflict

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -20,6 +20,10 @@ public class PlaneController : MonoBehaviour
 
     public GameObject throttleObject;
     public GameObject flightStickObject;
+    
+
+    public AudioSource audioSource;
+    public AudioListener audioListener;
     #endregion
 
     #region Private_Members
@@ -32,6 +36,8 @@ public class PlaneController : MonoBehaviour
 
     private Rigidbody jetBody;
 
+    private Vector3 flightStickRotation;
+    private Vector3 delta;
     #endregion
 
     #region TestVariables
@@ -48,7 +54,8 @@ public class PlaneController : MonoBehaviour
     void Start()
     {
         jetBody = GetComponent<Rigidbody>();
-        
+        audioSource.GetComponent<AudioSource>();
+
     }
 
     private void Update()
@@ -58,21 +65,29 @@ public class PlaneController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        alignPlaneWithHMD();
+        audioSource.Play();
+
         if (throttle.tracking)
         {
             speedGoal = maxSpeedPerSec * throttle.getThrottleValue();
+            
+            
         }
 
         if (joyStick.tracking)
         {
-            Vector3 delta = joyStick.getRelativePosition();
+            delta = joyStick.getRelativePosition();
             float pitchPercentage = delta.x / 90;
             float rollPercentage = delta.y / 90;
+            
             
             currentRotationPerSecond = new Vector3(
                 maxRotationPerSecond.x * pitchPercentage * Time.deltaTime,
                 0,
                 maxRotationPerSecond.z * rollPercentage * Time.deltaTime);
+
         }
         else
         {
@@ -96,6 +111,10 @@ public class PlaneController : MonoBehaviour
     {
         //jetBody.MoveRotation(Quaternion.Euler(currentRotationPerSecond));
         jetBody.rotation *= Quaternion.Euler(currentRotationPerSecond);
+        //flightStickObject.transform.rotation = Quaternion.Euler(currentRotationPerSecond);
+        calculateFlightStickAdjustmentVector(delta);
+     
+        
         speedGoal = Mathf.Abs(speedGoal);
         
         if (speedGoal > currentSpeed)
@@ -103,11 +122,18 @@ public class PlaneController : MonoBehaviour
             Debug.Log(speedGoal);
             currentSpeed += accelerationPerSecond;
             if (currentSpeed > maxSpeedPerSec) currentSpeed = maxSpeedPerSec;
+            
+            // throttleAdjustment
+            throttleObject.transform.rotation = Quaternion.Euler(new Vector3(calculateThrottleAdjustmentAngle(throttle.getThrottleValue()),0,0)); // adjust angle
         }
 
         Vector3 forward = jetBody.transform.forward;
         forward = forward * currentSpeed * Time.fixedDeltaTime;
+       
+        
+        
         jetBody.MovePosition((jetBody.position + forward));
+        
         //jetBody.velocity = new Vector3(0, 0, currentSpeed);
     }
 
@@ -127,6 +153,39 @@ public class PlaneController : MonoBehaviour
         cameraZeroRotation = player.transform.rotation;
         aligned = true;
     }
+    
+    private float calculateThrottleAdjustmentAngle(float throttleValue)
+    {
+        return ((-90 * throttleValue) -90); // Calculate Angle of Throttle
+    }
+
+    private void calculateFlightStickAdjustmentVector(Vector3 rotatedFlightStickVector3)
+    {
+        /* NOTES:
+         We need the if statements, because the physic of the flightstick is kinda wierd. It simply has to do with the rotation of a sphere but with parametes of an 'stick'
+         */
+        Vector3 flightStickadjustment;
+        if (rotatedFlightStickVector3.x != 0 && rotatedFlightStickVector3.y == 0)  // only x axis is involved
+        {
+            flightStickadjustment.x = (-90 + rotatedFlightStickVector3.x);
+            flightStickadjustment.y = 0;
+            flightStickadjustment.z = 0;
+            
+        } else if (rotatedFlightStickVector3.x == 0 && rotatedFlightStickVector3.y != 0) // only y axis is involved
+        {
+            flightStickadjustment.x = (-90 + rotatedFlightStickVector3.y);
+            flightStickadjustment.y = 90;
+            flightStickadjustment.z = -90;
+        }
+        else // x and y axis participate 
+        {
+            flightStickadjustment.x = (-90 + rotatedFlightStickVector3.x);
+            flightStickadjustment.y = rotatedFlightStickVector3.y;
+            flightStickadjustment.z = flightStickadjustment.y * (-1);
+        }
+        flightStickObject.transform.rotation = Quaternion.Euler(flightStickadjustment);
+    }
+    
     
     #endregion
 
