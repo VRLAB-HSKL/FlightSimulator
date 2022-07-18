e119a911713aa10f264ca4767785403c32206eaa
KattlerJoe <paka0010@stud.hs-kl.de>
2022-07-18 13:57:34 +0200

    schinken
    vegan

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    #region variables
 
     public Sound[] sounds;
-    
-    // Start is called before the first frame update
+
+    #endregion
+
+    #region unity lifecycle
+
     void Awake()
     {
         foreach (Sound s in sounds)
@@ -20,6 +24,10 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    #endregion
+
+    #region methods
+
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name); // find the right sound, where sound is equal to the used name
@@ -29,5 +37,19 @@ public class AudioManager : MonoBehaviour
             
         }
         s.source.Play();
-    } 
-}
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name); // find the right sound, where sound is equal to the used name
+        if (s == null)
+        {
+            return; // if there is any problem with the soundfile
+            
+        }
+        s.source.Stop();
+    }
+    
+    #endregion
+    
+}// End class
